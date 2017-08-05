79bd5aeff34a2e634703f6aa7ff920b5d93afcf9
Benny A <virusdownloader7@gmail.com>
2017-08-04 20:11:06 -0700

    Added potatowmrc support

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ extern crate px;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
+extern crate xdg;
 
 mod wm;
 mod logs;
+
+use std::process;
+use std::path::Path;
 
 use wm::WM;
 
@@ -25,6 +29,26 @@ fn main() {
 	let events = px::conn::EventLoop::new(&conn).unwrap();
 	let mut potato = WM::new(&conn, events);
 	
+	let basedirs = xdg::BaseDirectories::new().unwrap();
+	let rcdir = basedirs.get_config_home().join(Path::new("potato"));
+	let potatowmrc = basedirs.find_config_file(Path::new("potato/potatowmrc"));
+
+	if let Some(rc) = potatowmrc {
+		let setup_script = process::Command::new("sh")
+				.stdout(process::Stdio::null())
+				.current_dir(rcdir)
+				.args(&["-c", rc.to_str().unwrap()])
+				.spawn();
+		
+		match setup_script {
+			Ok(_) => { info!("Configured potatowm"); }
+			Err(e) => { error!("Couldn't execute potatowmrc: {}", e); },
+		}
+
+	} else {
+		warn!("Couldn't find potatowmrc in {}", rcdir.to_string_lossy());
+	}
+
 	match potato.run() {
 		Ok(_) => {
 			info!("Exited successfully");
